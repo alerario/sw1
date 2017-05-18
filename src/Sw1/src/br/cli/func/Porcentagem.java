ed8f3d2e9c1eabb04ee146b6910014ce96951bc1
alerario <lua_ivo@hotmail.com>
2017-05-18 20:35:02 -0300

    porcentagem lua

---
+++ src/Sw1/src/br/cli/func/Porcentagem.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.cli.func;
+
+/**
+ *
+ * @author alerario
+ */
+public class Porcentagem {
+    public float calcularPorcentagem (int a, int b){
+        return a*(b/100);
+    }
+}
