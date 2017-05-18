3224625562e1cb234d95c1b2dfd8999d97bfe3c0
andressalhenriques <andressalhenriques@gmail.com>
2017-05-18 20:38:02 -0300

    Andressaa

---
+++ src/Sw1/src/br/cli/gui/GuiOrdenacao.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.cli.gui;
+
+/**
+ *
+ * @author Andressa
+ */
+public class GuiOrdenacao {
+    
+    public void Ordenacao() {
+
+        System.out.println("Numeros Ordenados");
+  
+    }
+}
