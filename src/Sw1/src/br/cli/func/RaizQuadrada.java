a5b34b1a72b5d944fe1b15b9fd248b84718b7617
Carlos <carlos.caulin36@gmail.com>
2017-05-18 20:25:25 -0300

    Raiz Feita

---
+++ src/Sw1/src/br/cli/func/RaizQuadrada.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.cli.func;
+
+/**
+ *
+ * @author carlos
+ */
+public class RaizQuadrada {
+    public static double RaizQuadrada(int x){
+        Math.sqrt(x);
+        return x;
+        
+    }
+    
+}
