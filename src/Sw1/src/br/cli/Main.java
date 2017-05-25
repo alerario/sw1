2a7ddbdb377fe14f9c85ff13147828a0a7dbdb19
Caroline Rodrigues Amaro Correa <caroline.amaro@hotmail.com>
2017-05-25 02:06:04 -0300

    correcao das mesclagens incorretas

--- src/Sw1/src/br/cli/Main.java
+++ src/Sw1/src/br/cli/Main.java
@@ -1,30 +1,13 @@
-<<<<<<< HEAD
-=======
-<<<<<<< HEAD
->>>>>>> a2e899bd0d8796f256a1eac1db437e345576396c
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package br.cli;
 
-<<<<<<< HEAD
-=======
-import br.cli.gui.GuiCalcularMedia;
-import br.cli.gui.GuiParOuImpar;
-import br.cli.gui.GuiPotenciacao;
-import br.cli.gui.GuiRaizCubica;
-import br.cli.gui.GuiSomatorio;
+import br.cli.gui.*;
 
->>>>>>> a2e899bd0d8796f256a1eac1db437e345576396c
 /**
  *
  * @author utfpr
  */
 public class Main {
-<<<<<<< HEAD
-=======
 
     public static void main(String[] args) {
         System.out.println("Bem vindo.");
@@ -37,230 +20,57 @@ public class Main {
             if (op == 1) {
                 new br.cli.gui.GuiSomar().somar2numeros();
             }
-            if (op == 3){
+            else if (op == 3){
                 new br.cli.gui.GuiMaiorMenor().maiorMenor();
             }
-
-            if (op == 19) {
+            else if (op == 19) {
                 new GuiParOuImpar();
             }
-            if (op == 21) {
+            else if (op == 21) {
                 new GuiSomatorio();
             }
-            if (op == 25) {
+            else if (op == 25) {
                 new GuiCalcularMedia();
             }
-
-            if (op == 44) {
+            else if (op == 44) {
                 new br.cli.gui.GuiFubonacci().CalculaFib();
             }
-            if (op == 22) {
+            else if (op == 22) {
                 new br.cli.gui.GuiFuncaoExponencial().elevarNumero();
             }
-             if (op == 24) {
+            else if (op == 24) {
                 new br.cli.gui.GuiPorcentagem();
             }
-            
-            if (op == 17) {
+            else if (op == 17) {
                 new br.cli.gui.GuiDistPontos().calcularDistancia();
             }
-             if(op==7){
+            else if(op==7){
                 new br.cli.gui.GuiFatorial().fatorial();
             }
-            
-            if(op==8){
+            else if(op == 14){
+                new br.cli.gui.GuiMultiplicarMuitosNumeros().getGUI();
+            }
+            else if(op==8){
                new br.cli.gui.GuiSubtrairnNumeros().subtrairnumeros();
-            }
-            if(op==33){
-               new br.cli.gui.GuiOrdenacao().ordenacao();
-            }
-            if(op==26){
-               new GuiPotenciacao();
-            }
-            if(op==27){
-               new GuiRaizCubica();
-            }
-            
-            
-            
-            menu();
-            op = new java.util.Scanner(System.in).nextInt();
-        }
-        System.out.println("$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$");
-        System.out.println("Fim do programa!");
-    }
-
-    public static void menu() {
-        System.out.println("###########################################");
-        System.out.println("0 - sair");
-        System.out.println("1 - somar dois numeros");
-        System.out.println("2 - subtrair dois numeros");
-        System.out.println("3 - Informar o maior e o menor número");
-        System.out.println("7 - fatorial de um numero");
-        System.out.println("8 - subtrair n numeros");
-        System.out.println("22 - Exponencial");
-        System.out.println("44 - Calcular Fibonacci");
-        System.out.println("17 - Calcular Distancia entre pontos");
-        System.out.println("19 - Informa um número para verificar se é par ou impar? ");
-        System.out.println("21 - Informa um número para o somatorio? "); //calcula de 1 ate o numero informado
-        System.out.println("25 - Calcular a média entre dois números? ");
-        System.out.println("24 - Calcular a porcentagem de um número");//calcula a media entre 2 numeros
-        System.out.println("26 - Potencia");
-        System.out.println("27 - Raiz cubica de um numero");
-        System.out.println("33 - Ordenar numero aleatório");
-    }
-
-}
-/*
-
-package br.cli;
-
-public class Main {
->>>>>>> a2e899bd0d8796f256a1eac1db437e345576396c
-    public static void main(String[] args) {
-        System.out.println("Bem vindo.");
-        System.out.println("==>selecione as opcoes");
-        int op;
-        menu();
-        op = new java.util.Scanner(System.in).nextInt();
-        while(op!=0){
-            
-            if(op==1){
-                new br.cli.gui.GuiSomar().somar2numeros();
-<<<<<<< HEAD
             }
             else if(op == 20){
                 new br.cli.gui.GuiMultiplicarTresNumeros().multiplicarTres();
             }
-=======
-            }else if(op == 14){
-                new br.cli.gui.GuiMultiplicarMuitosNumeros().getGUI();
-            }
-            if (op == 44) {
-                new br.cli.gui.GuiFubonacci().CalculaFib();
-            }
-            if (op == 22) {
-                new br.cli.gui.GuiFuncaoExponencial().elevarNumero();
-            }
-<<<<<<< HEAD
-            
-            if(op==7){
-                new br.cli.gui.GuiFatorial().fatorial();
-            }
-            
-            if(op==8){
-                new br.cli.gui.GuiSubtrairnNumeros().subtrairnumeros();
-            }
-            
-=======
->>>>>>> f20013145446a8763fc73d435ea108c679e6a6d8
->>>>>>> a2e899bd0d8796f256a1eac1db437e345576396c
-            menu();
-            op = new java.util.Scanner(System.in).nextInt();
-        }
-    }
-    
-    public static void menu(){
-        System.out.println("##################################");
-        System.out.println("0 - sair");
-        System.out.println("1 - somar dois numeros");
-        System.out.println("2 - subtrair dois numeros");
-<<<<<<< HEAD
-        System.out.println("20 - Multiplicar Tres numeros");
-=======
-<<<<<<< HEAD
-        System.out.println("7 - fatorial de um numero");
-        System.out.println("8 - subtrair n numeros");
-=======
-        System.out.println("14 - Multiplicar N números");
-        System.out.println("22 - Exponencial");
-        System.out.println("44 - Calcular Fibonacci");
-
->>>>>>> f20013145446a8763fc73d435ea108c679e6a6d8
->>>>>>> a2e899bd0d8796f256a1eac1db437e345576396c
-    }
-    
-    
-}
-<<<<<<< HEAD
-=======
->>>>>>> 477aa08be892a68b019e4c9f0d924945873560a1
- */
-=======
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-package br.cli;
-
-import br.cli.gui.GuiCalcularMedia;
-import br.cli.gui.GuiParOuImpar;
-import br.cli.gui.GuiPotenciacao;
-import br.cli.gui.GuiSomatorio;
-
-/**
- *
- * @author utfpr
- */
-public class Main {
-
-    public static void main(String[] args) {
-        System.out.println("Bem vindo.");
-        System.out.println("==>selecione as opcoes");
-        int op;
-        menu();
-        op = new java.util.Scanner(System.in).nextInt();
-        while (op != 0) {
-
-            if (op == 1) {
-                new br.cli.gui.GuiSomar().somar2numeros();
-            }
-            if (op == 3){
-                new br.cli.gui.GuiMaiorMenor().maiorMenor();
-            }
-
-            if (op == 19) {
-                new GuiParOuImpar();
-            }
-            if (op == 21) {
-                new GuiSomatorio();
-            }
-            if (op == 25) {
-                new GuiCalcularMedia();
-            }
-
-            if (op == 44) {
-                new br.cli.gui.GuiFubonacci().CalculaFib();
-            }
-            if (op == 22) {
-                new br.cli.gui.GuiFuncaoExponencial().elevarNumero();
-            }
-             if (op == 24) {
-                new br.cli.gui.GuiPorcentagem();
-            }
-            
-            if (op == 17) {
-                new br.cli.gui.GuiDistPontos().calcularDistancia();
-            }
-             if(op==7){
-                new br.cli.gui.GuiFatorial().fatorial();
-            }
-            
-            if(op==8){
-               new br.cli.gui.GuiSubtrairnNumeros().subtrairnumeros();
-            }
-            if(op==33){
+            else if(op==33){
                new br.cli.gui.GuiOrdenacao().ordenacao();
             }
-            if(op==26){
+            else if(op==26){
                new GuiPotenciacao();
             }
-             if(op==55){
+            else if(op==27){
+               new GuiRaizCubica();
+            }
+            else if(op == 28){
+                new GuiRaizQuadrada();
+            }
+            else if(op==55){
                new br.cli.gui.GuiAleatorio().aleatorio();
             }
-            
-            
             
             menu();
             op = new java.util.Scanner(System.in).nextInt();
@@ -277,79 +87,20 @@ public class Main {
         System.out.println("3 - Informar o maior e o menor número");
         System.out.println("7 - fatorial de um numero");
         System.out.println("8 - subtrair n numeros");
-        System.out.println("22 - Exponencial");
-        System.out.println("44 - Calcular Fibonacci");
+        System.out.println("14 - Multiplicar N números");
         System.out.println("17 - Calcular Distancia entre pontos");
         System.out.println("19 - Informa um número para verificar se é par ou impar? ");
-        System.out.println("21 - Informa um número para o somatorio? "); //calcula de 1 ate o numero informado
-        System.out.println("25 - Calcular a média entre dois números? ");
+        System.out.println("20 - Multiplicar Tres numeros");
+        System.out.println("21 - Informa um número para o somatorio? "); //calcula de 1 ate o numero informado  
+        System.out.println("22 - Exponencial");
         System.out.println("24 - Calcular a porcentagem de um número");//calcula a media entre 2 numeros
+        System.out.println("25 - Calcular a média entre dois números? ");
         System.out.println("26 - Potencia");
+        System.out.println("27 - Raiz cubica de um numero");
+        System.out.println("28 - Raiz quadrada de um numero");
         System.out.println("33 - Ordenar numero aleatório");
+        System.out.println("44 - Calcular Fibonacci");
         System.out.println("55 - numero aleatório");
     }
-
 }
-/*
 
-package br.cli;
-
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("Bem vindo.");
-        System.out.println("==>selecione as opcoes");
-        int op;
-        menu();
-        op = new java.util.Scanner(System.in).nextInt();
-        while(op!=0){
-            
-            if(op==1){
-                new br.cli.gui.GuiSomar().somar2numeros();
-            }else if(op == 14){
-                new br.cli.gui.GuiMultiplicarMuitosNumeros().getGUI();
-            }
-            if (op == 44) {
-                new br.cli.gui.GuiFubonacci().CalculaFib();
-            }
-            if (op == 22) {
-                new br.cli.gui.GuiFuncaoExponencial().elevarNumero();
-            }
-<<<<<<< HEAD
-            
-            if(op==7){
-                new br.cli.gui.GuiFatorial().fatorial();
-            }
-            
-            if(op==8){
-                new br.cli.gui.GuiSubtrairnNumeros().subtrairnumeros();
-            }
-            
-=======
->>>>>>> f20013145446a8763fc73d435ea108c679e6a6d8
-            menu();
-            op = new java.util.Scanner(System.in).nextInt();
-        }
-    }
-    
-    public static void menu(){
-        System.out.println("##################################");
-        System.out.println("0 - sair");
-        System.out.println("1 - somar dois numeros");
-        System.out.println("2 - subtrair dois numeros");
-<<<<<<< HEAD
-        System.out.println("7 - fatorial de um numero");
-        System.out.println("8 - subtrair n numeros");
-=======
-        System.out.println("14 - Multiplicar N números");
-        System.out.println("22 - Exponencial");
-        System.out.println("44 - Calcular Fibonacci");
-
->>>>>>> f20013145446a8763fc73d435ea108c679e6a6d8
-    }
-    
-    
-}
->>>>>>> 477aa08be892a68b019e4c9f0d924945873560a1
- */
->>>>>>> origin/master
->>>>>>> a2e899bd0d8796f256a1eac1db437e345576396c
