128aebe86b32d5f6f234b415a30045b71e25f830
Luiz Fernando Consalter Fonçatti <luifon@alunos.utfpr.edu.br>
2017-06-22 16:19:22 -0300

    Add files via upload
    Verifica se um numero é positivo ou negativo.

---
+++ src/Sw1/src/br/cli/func/PostivoOuNegativo.java
@@ -0,0 +1,15 @@
+package sw1;
+
+/**
+ *
+ * @author luiz-
+ */
+public class PostivoOuNegativo {
+    public void positivoOuNegativo(int n){
+        if (n < 0){
+            System.out.println("Numero negativo.");
+        } else {
+            System.out.println("Numero positivo.");
+        }
+    }
+}
